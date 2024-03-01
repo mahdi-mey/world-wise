e65a99e6194e584c9e8c717127e657e32569ee08
Mahdi <mahdimey17@gmail.com>
2024-03-01 16:53:43 +0330

    protected app from unauthorized access

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
-import Products       from  "./pages/Product"
-import Pricing        from  "./pages/Pricing"
-import Homepage       from  "./pages/Homepage"
-import PageNotFound   from  "./pages/PageNotFound"
-import Login          from  "./pages/Login"
-import AppLayout      from  "./pages/AppLayout"
-import CityList       from  "./components/CityList"
-import CountryList    from  "./components/CountryList"
-import City           from  "./components/City"
-import Form           from  "./components/Form"
+import Products from "./pages/Product"
+import Pricing from "./pages/Pricing"
+import Homepage from "./pages/Homepage"
+import PageNotFound from "./pages/PageNotFound"
+import Login from "./pages/Login"
+import AppLayout from "./pages/AppLayout"
+import CityList from "./components/CityList"
+import CountryList from "./components/CountryList"
+import City from "./components/City"
+import Form from "./components/Form"
 
 import { CitiesProvider } from "./contexts/CitiesContext"
-import { AuthProvider   } from './contexts/FakeAuthContext'
+import { AuthProvider } from "./contexts/FakeAuthContext"
+import ProtectedRoute from "./pages/ProtectedRoute"
 
 function App() {
   return (
@@ -25,17 +21,24 @@ function App() {
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/"             element={<Homepage   />} />
-            <Route path="products"      element={<Products   />} />
-            <Route path="pricing"       element={<Pricing    />} />
-            <Route path="login"         element={<Login      />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="products" element={<Products />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
 
-            <Route path="app"           element={<AppLayout   />}>
-              <Route index              element={<Navigate to="cities" replace />} />
-              <Route path="cities"      element={<CityList    />} />
-              <Route path="cities/:id"  element={<City        />} />
-              <Route path="countries"   element={<CountryList />} />
-              <Route path="form"        element={<Form        />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
             </Route>
 
             <Route path="*" element={<PageNotFound />} />
---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+import { useAuth } from "../contexts/FakeAuthContext"
+import { useNavigate } from "react-router-dom"
+
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(function () {
+    if (!isAuthenticated) navigate("/")
+  }, [])
+
+  return isAuthenticated ? children : null
+}
